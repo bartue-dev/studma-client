667d18aec6f1adacebabc802e76f6600ffa00342
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-09 15:46:40 +0800

    Import zod from zod and useForm from react-hook-form as well as the zodResolver. Input the LoginSchema using zod and get the data and validate before passing to the login queryfrom the form using useForm. Implements proper error handling using the errors from useForm

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,59 +1,54 @@
-import { useState, type FormEvent } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { useNavigate, Link } from "react-router-dom"
 
 import { useAppDispatch } from "../../features/hooks"
 import { setCredentials } from "../../features/authSlice"
 import { useLoginMutation } from "../../features/authApiSlice"
 
-type ApiError = {
-  status: number,
-}
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
 
+//server error types
 type ErrorTypes = {
-  username?: string,
-  password?: string,
-  server?: string
+  error?: string
 } | string
 
+//zod schema
+const LoginSchema = z.object({
+  username: z.string().min(2, "Username must not be empty"),
+  password: z.string().min(2, "Password must not be empty")
+});
+
+//infer zod schema to be a type
+type LoginData = z.infer<typeof LoginSchema>;
+
+//Login component
 export default function Login() {
-  const [error, setError] = useState<ErrorTypes>({});
+  const { register, handleSubmit, formState: {errors}} = useForm<LoginData>({
+    resolver: zodResolver(LoginSchema),
+  })
+  
+  const [serverError, setServerError] = useState<ErrorTypes>({});
   const navigate = useNavigate();
 
   const [login, { isLoading }] = useLoginMutation();
   const dispatch = useAppDispatch();
 
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (data: LoginData) => {
 
     try {
 
-      const inputErr: ErrorTypes = {}
-
-      const formData = new FormData(e.currentTarget);
-
-      const username = formData.get("username") as string;
-      const password = formData.get("password") as string;
-
-      if (username.length <= 0) {
-        inputErr.username = "Username must not be empty";
-      }
-
-      if (password.length <= 0) {
-        inputErr.password = "Password must not be empty";
-      }
-
-      if (Object.keys(inputErr).length > 0) {
-        setError(inputErr)
-        return;
-      }
-
-      const userData = await login({ username, password}).unwrap();
+      const userData = await login({ 
+        username: data.username,
+        password: data.password
+      }).unwrap();
 
       const fullname: string = userData.fullname;
       const accessToken: string = userData.accessToken;
 
-      dispatch(setCredentials({username, fullname, accessToken}));
+      dispatch(setCredentials({ fullname, accessToken}));
 
       console.log("USER DATA:", userData)
 
@@ -61,64 +56,80 @@ export default function Login() {
     } catch (err) {
       console.error(err)
 
-      const error = err as ApiError
+      const error = err as {status: number}
       const serverError: ErrorTypes = {}
 
       if (!error?.status) {
-        serverError.server = "No server response";
+        serverError.error = "No server response";
       } else if (error.status === 400) {
-        serverError.server = "Incorrect username or password";
+        serverError.error = "Incorrect username or password";
       } else if (error.status === 401) {
-        serverError.server = "Unauthorized";
+        serverError.error = "Unauthorized";
       } else {
-        serverError.server = "Login Failed";
+        serverError.error = "Login Failed";
       }
 
       if (Object.keys(serverError).length > 0) {
-        setError(serverError)
+        setServerError(serverError)
         return
       }
     }
   }
 
   return (
-    <div className="border border-gray-400 m-auto mt-15 w-80 p-10 rounded-md ">
-      {typeof error === "object" && error?.server && <p className="text-sm text-red-500">{error.server}</p>}
-      <form 
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-5"  
-      >
-        <div className="flex flex-col justify-center gap-1">
-          <label htmlFor="username">Username </label>
-          <input 
-            type="text" 
-            name="username" 
-            id="username"
-            className="input input-sm"
-            />
-          {typeof error === "object" && error?.username && <p className="text-sm text-red-500">{error?.username}</p>}
-        </div>
+  <div className="font-Sans justify-items-center">
+    <form 
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-5 w-100 px-10 py-8 mt-15 shadow-sm border border-gray-100 rounded-md"  
+    >
+      <h1 className="text-xl text-center font-semibold">Student Management System</h1>
 
-        <div className="flex flex-col justify-center gap-1">
-          <label htmlFor="password">Password </label>
-          <input 
-            type="password" 
-            name="password" 
-            id="password"
-            className="input input-sm"
-            />
-          {typeof error === "object" && error?.password && <p className="text-sm text-red-500">{error?.password}</p>}
-        </div>
-      
-        <button 
-          type="submit"
-          className="place-self-start btn btn-primary w-30"
-          disabled={isLoading ? true : false}
+        {/* server error message */}
+      {typeof serverError === "object" && serverError?.error && <p className="text-sm text-red-500">{serverError.error}</p>}
+
+      <div className="flex flex-col justify-center gap-1">
+        <label 
+          htmlFor="username"
+          className="text-base"
         >
-          { isLoading && <span className="loading loading-spinner text-neutral"></span> }
-          Submit
-        </button>
-      </form>
-    </div>
+          Username
+        </label>
+        <input 
+          type="text" 
+          id="username"
+          {...register("username")}
+          className="input"
+          />
+        {errors?.username && <p className="text-sm text-red-500">{errors?.username?.message}</p>}
+      </div>
+
+      <div className="flex flex-col justify-center gap-1">
+        <label 
+          htmlFor="password"
+          className="text-base"
+        >
+          Password
+        </label>
+        <input 
+          type="password" 
+          id="password"
+          {...register("password")}
+          className="input"
+          />
+        {errors?.password && <p className="text-sm text-red-500">{errors?.password?.message}</p>}
+      </div>
+    
+      <button 
+        type="submit"
+        className="place-self-start btn btn-primary w-full text-white"
+        disabled={isLoading ? true : false}
+      >
+        { isLoading && <span className="loading loading-spinner text-neutral"></span> }
+        Submit
+      </button>
+
+      <p className="text-sm">No account? <Link to="/register" className="link link-primary">Create an account</Link></p>
+    </form>
+  </div>
   )
 }
