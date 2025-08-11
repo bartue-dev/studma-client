facf9984552e4bb9a309bbd8e82072df51c76b2f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-11 20:44:38 +0800

    Add a initial table from shadcn

--- src/Pages/Attendance.tsx
+++ src/Pages/Attendance.tsx
@@ -1,8 +1,39 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 export default function Attendance() {
   return (
-    <div>
-      <h1>THIS IS ATTENDANCE</h1>
+    <div className="h-full">
+      {/* <h1>THIS IS ATTENDANCE</h1> */}
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[150px]">STUDENT NAME</TableHead>
+            <TableHead>GRADE</TableHead>
+            <TableHead>SECTION</TableHead>
+            <TableHead className="">STATUS</TableHead>
+            <TableHead className="">PREVIOUS 7 DAYS STATUS</TableHead>
+            <TableHead className="text-right">ABSENT DAYS</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   )
 }
