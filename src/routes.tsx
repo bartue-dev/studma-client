9c464981b65513e60339f5353c8d522c21df61cc
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-11 17:29:17 +0800

    Import the inital Links components

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,17 @@ import RequiredAuth from "./components/Common/RequiredAuth";
 import PersistLogin from "./components/Common/PersistLogin";
 import Register from "./components/Auth/Register";
 import Attendance from "./Pages/Attendance";
+import NotFoundPage from "./components/Common/404Error";
+import Students from "./Pages/Students";
+import Batch from "./Pages/Batch";
+import Course from "./Pages/Course";
+import Section from "./Pages/Section";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    errorElement: <NotFoundPage/>,
     children: [
 
       {element: <PersistLogin/>,
@@ -20,14 +26,18 @@ export const router = createBrowserRouter([
               {path: "home", element: <Home/>,
                 children: [
                   {index: true, element: <Attendance/>},
-                  {path: "attendance", element: <Attendance/>}
+                  {path: "attendance", element: <Attendance/>},
+                  {path: "students", element: <Students/>},
+                  {path: "batch", element: <Batch/>},
+                  {path: "course", element: <Course/>},
+                  {path: "section", element: <Section/>},
+
                 ]
               }
             ]
           }
         ]
-      }
-      
+      } 
     ]
   },
   {path: "login", element: <Login/>},
