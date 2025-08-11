c174eb1cee8edf1e40ff8d167c638c38065e0208
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-11 17:26:51 +0800

    Edit the items properties for sidebar links. Then added a logout function using the logout query from useLogoutMutation. Then dispatch the logout action to set the credentials to null. Edit the sidebar, added title and footer. The footer contain a logout button. Then change the size of the links text and icons

--- src/components/Common/AppSidebar.tsx
+++ src/components/Common/AppSidebar.tsx
@@ -1,60 +1,82 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { CalendarDays, ListChecks , GraduationCap , NotebookPen , Signpost , LogOut, NotebookText   } from "lucide-react"
+
+import { Link } from "react-router-dom"
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
+import { useLogoutMutation } from "@/features/Auth/authApiSlice";
+import { useAppDispatch } from "@/features/hooks";
+import { logOut } from "@/features/Auth/authSlice";
+
 // Menu items.
 const items = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    title: "Attendance",
+    url: "/home/attendance",
+    icon: ListChecks,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    title: "Students",
+    url: "/home/students",
+    icon: GraduationCap ,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    title: "Batch",
+    url: "/home/batch",
+    icon: CalendarDays,
   },
   {
-    title: "Search",
-    url: "#",
-    icon: Search,
+    title: "Course",
+    url: "/home/course",
+    icon: NotebookPen ,
   },
   {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: "Sections",
+    url: "/home/section",
+    icon: Signpost ,
   },
 ]
 
 export function AppSidebar() {
+  const [logout] = useLogoutMutation();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = async () => {
+    await logout(undefined);
+    dispatch(logOut());
+  }
+
   return (
     <Sidebar>
-      <SidebarContent>
+      <SidebarHeader 
+        className="bg-sky-600 text-white text-xl font-bold p-4"
+      >
+       <div className="flex items-center gap-2">
+         <NotebookText />
+          STUDMA
+       </div>
+      </SidebarHeader>
+      <SidebarContent className="bg-gray-800 text-white">
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="flex fexl-col justify-center gap-2">
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
+                  <SidebarMenuButton asChild className="text-base">
+                    <Link to={item.url}>
+                      <item.icon style={{ width: '20px', height: '20px' }} />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
@@ -62,6 +84,23 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter className="bg-gray-800 text-white">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton 
+              className="cursor-pointer text-base"
+            >
+              <LogOut style={{ width: '20px', height: '20px' }} />
+              <span
+                className="cursor-pointer"
+                onClick={handleLogout}
+              >
+                Logout
+              </span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
