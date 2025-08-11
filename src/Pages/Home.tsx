e6e93e8bca85dff40eaa4fb1381b1b2b220ebfe3
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-11 20:45:12 +0800

    Add input search and and add styling

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,15 +3,40 @@ import SidebarLayout from "../components/Common/SidebarLayout";
 import { useAppSelector } from "@/features/hooks";
 import { selectCurrentUser } from "@/features/Auth/authSlice";
 
+import { User, Search } from 'lucide-react';
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   const fullname = useAppSelector(selectCurrentUser);
 
   return(
-    <div className="font-Sans">
+    <div className="font-Sans h-screen">
       <SidebarLayout>
-        <div>
-          <div className="py-4 bg-gray-100">{fullname}</div>
-          <Outlet/>
+        <div className="flex flex-col h-full">
+
+          {/* header */}
+          <div 
+            className="h-16 px-10 bg-gray-100 flex items-center justify-between"
+          >
+            <div className="relative">
+              <Search size={20} className="text-gray-600 absolute left-2 top-2"/>
+              <Input 
+                type="text"
+                name="search"
+                placeholder="Search student by name"
+                className="w-80 bg-white shadow-none pl-8"
+              />
+            </div>
+            <div className="flex items-center gap-3">
+              <span>Hi, Ma'am {fullname?.split(" ")[0]}</span>
+              <User size={25} className="text-gray-500"/>
+            </div>
+          </div>
+
+          {/* content */}
+          <div className="flex-1 p-8">
+            <Outlet/>
+          </div>
         </div>
       </SidebarLayout>
     </div>
