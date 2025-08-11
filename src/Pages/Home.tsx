0f00006608090625a75d2aa941f96d1a99d4e339
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-11 17:22:39 +0800

    Import useAppSelect and selectCurrentUser and render to the page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import SidebarLayout from "../components/Common/SidebarLayout";
+import { useAppSelector } from "@/features/hooks";
+import { selectCurrentUser } from "@/features/Auth/authSlice";
 
 export default function Home() {
+  const fullname = useAppSelector(selectCurrentUser);
+
   return(
-    <div>
+    <div className="font-Sans">
       <SidebarLayout>
         <div>
+          <div className="py-4 bg-gray-100">{fullname}</div>
           <Outlet/>
         </div>
       </SidebarLayout>
