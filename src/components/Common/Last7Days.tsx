adcbea7e4ea5f3bbd2a1afcf813debb48a8d2caa
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-13 23:34:03 +0800

    Creates a last7Days components. Rendering the last 7 days from current date and check if student is present absend late or excuse that days

---
+++ src/components/Common/Last7Days.tsx
@@ -0,0 +1,53 @@
+import { format } from "date-fns"
+import { Check, X, ClockAlert, Handshake, Ellipsis  } from "lucide-react";
+
+type AttendanceDateType = {
+  attendanceDate: {
+    attendanceDateId: string,
+    date: string,
+    status: string
+  }[]
+}
+
+
+
+export default function LastSevenDays({ attendanceDate }: AttendanceDateType) {
+
+  const getLast7Days = () => {
+    const dates = [];
+    for (let i = 0; i < 7; i++) {
+      const d = new Date(); // Get current date
+      d.setDate(d.getDate() - i); // Subtract 'i' days
+      const date = (format(d, "yyyy-MM-dd"));
+
+      const status = attendanceDate.find(data => data.date === date)?.status || null;
+
+      dates.push({
+        date,
+        status
+      })
+    }
+    return dates.reverse(); // Reverse to get chronological order (oldest to newest)
+  } 
+
+  console.log(getLast7Days())
+
+  return (
+    <div>
+      <div className="flex items-center gap-2">
+        {getLast7Days().map((data, i) => (
+          <div key={i} className="text-sm justify-items-center p-1">
+            <h1>{data.date.split("-")[2]}</h1>
+            <span>
+              { data.status === "PRESENT" ? <Check className="text-green-500" size={15}/> :
+                data.status === "ABSENT" ? <X className="text-red-500" size={15}/> :
+                data.status === "LATE" ? <ClockAlert className="text-yellow-500" size={15}/> :
+                data.status === "EXCUSE" ? <Handshake className="text-blue-500" size={15}/> :
+                <Ellipsis className="text-gray-600" size={15} />}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
