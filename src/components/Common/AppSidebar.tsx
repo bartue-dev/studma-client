684e84bbabdb6e8d6601a7c35783ad5bfaa74ec4
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-13 23:32:20 +0800

    Add conditional styling using useLocation.pathname if the pathname is equals to the sidebar link then add styling

--- src/components/Common/AppSidebar.tsx
+++ src/components/Common/AppSidebar.tsx
@@ -1,6 +1,6 @@
-import { CalendarDays, ListChecks , GraduationCap , NotebookPen , Signpost , LogOut, NotebookText   } from "lucide-react"
+import { ListChecks , GraduationCap , LogOut, NotebookText   } from "lucide-react"
 
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 import {
   Sidebar,
@@ -30,26 +30,13 @@ const items = [
     url: "/home/students",
     icon: GraduationCap ,
   },
-  {
-    title: "Batch",
-    url: "/home/batch",
-    icon: CalendarDays,
-  },
-  {
-    title: "Course",
-    url: "/home/course",
-    icon: NotebookPen ,
-  },
-  {
-    title: "Sections",
-    url: "/home/section",
-    icon: Signpost ,
-  },
 ]
 
 export function AppSidebar() {
   const [logout] = useLogoutMutation();
   const dispatch = useAppDispatch();
+  const pathname = useLocation().pathname;
+
 
   const handleLogout = async () => {
     await logout(undefined);
@@ -59,22 +46,27 @@ export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader 
-        className="bg-sky-600 text-white text-xl font-bold h-16 px-4"
+        className="text-xl font-bold h-16 px-4 bg-white"
       >
-       <div className="flex items-center gap-2 h-full">
-         <NotebookText />
-          STUDMA
+       <div className="flex items-center gap-2 w-fit h-full">
+         <NotebookText className="text-purple-500"/>
+         <h1 className="text-purple-500">STUDMA</h1>
        </div>
       </SidebarHeader>
-      <SidebarContent className="bg-gray-800 text-white">
+      <SidebarContent className="bg-white border-b-1 border-t-1">
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu className="flex fexl-col justify-center gap-2">
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild className="text-base">
+                  <SidebarMenuButton 
+                    asChild 
+                    className={`text-base p-5 rounded-l-none 
+                      ${pathname === item.url && "bg-purple-500 text-white hover:bg-purple-500 hover:text-white"}`}>
                     <Link to={item.url}>
-                      <item.icon style={{ width: '20px', height: '20px' }} />
+                      <item.icon 
+                        style={{ width: '20px', height: '20px' }} 
+                        className={`text-gray-600 ${pathname === item.url && "text-white"}`}/>
                       <span>{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
@@ -84,13 +76,13 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter className="bg-gray-800 text-white">
+      <SidebarFooter className="bg-white ">
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton 
-              className="cursor-pointer text-base"
+              className="cursor-pointer text-base p-5"
             >
-              <LogOut style={{ width: '20px', height: '20px' }} />
+              <LogOut style={{ width: '20px', height: '20px' }} className="text-gray-600" />
               <span
                 className="cursor-pointer"
                 onClick={handleLogout}
