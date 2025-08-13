4e73af440b1ab5a3a52e49c85ce0a92ed5682b80
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-13 23:30:59 +0800

    Import the RTK query to retrieve the student data from the backedn

--- src/Pages/Attendance.tsx
+++ src/Pages/Attendance.tsx
@@ -8,32 +8,72 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import { BeatLoader } from "react-spinners";
+
+import { useGetAllStudentsQuery } from "@/features/Attendance/AttendanceApiSlice"
+import { StatusCombobox } from "@/components/Common/StatusCombobox"
+
+import LastSevenDays from "@/components/Common/Last7Days";
+
 
 export default function Attendance() {
+  const {
+    data: studentsData,
+    isSuccess,
+    isLoading,
+    isError,
+    error
+  } = useGetAllStudentsQuery();
+
+  if (isError) {
+    console.log(error)
+  }
+
   return (
     <div className="h-full">
-      {/* <h1>THIS IS ATTENDANCE</h1> */}
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[150px]">STUDENT NAME</TableHead>
-            <TableHead>GRADE</TableHead>
-            <TableHead>SECTION</TableHead>
-            <TableHead className="">STATUS</TableHead>
-            <TableHead className="">PREVIOUS 7 DAYS STATUS</TableHead>
-            <TableHead className="text-right">ABSENT DAYS</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="">$250.00</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <h1 className="mb-8 text-3xl text-gray-800 font-semibold">Daily attendance</h1>
+      <div className="border shadow-sm bg-white p-3 rounded-md">
+        <Table className="w-full h-full">
+          <TableCaption>List of Students</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[200px]">STUDENT NAME</TableHead>
+              <TableHead className="">PREVIOUS 7 DAYS STATUS</TableHead>
+              <TableHead >STATUS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {isLoading
+              ? <TableRow>
+                  <TableCell colSpan={6} className="text-center py-8 justify-items-center">
+                    <BeatLoader color="#5e9fe3"/>
+                  </TableCell>
+                </TableRow>
+              : isSuccess
+              && studentsData?.studentData.map(student => (
+                  <TableRow key={student.studentId}>
+                    <TableCell 
+                      className="font-medium"
+                    >
+                      {student.firstname} {student.lastname}
+                    </TableCell>
+                    <TableCell>
+                     <LastSevenDays attendanceDate={student?.attendanceDate}/>
+                      {/* {student?.attendanceDate.map(attendance => (
+                        <div key={attendance.attendanceDateId}>
+                          <h1>{attendance.date}</h1>
+                          <h1>{attendance.status}</h1>
+                        </div>
+                      ))} */}
+                    </TableCell>
+                    <TableCell>
+                    <StatusCombobox attendanceDate={student?.attendanceDate}/>
+                    </TableCell>
+                  </TableRow>
+                  ))} 
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
