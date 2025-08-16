a37ce06eefda9bac3995538293af58fcd515c27e
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-16 16:14:16 +0800

    Import combineReducers and UnknownAction from redux the use it to combine the all reducers so it would reset the state of all reducers when auth/logout is dispatch

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, type UnknownAction } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/api";
 import authReducer from "./features/Auth/authSlice";
 import studentReducer from "./features/Student/StudentSlice";
 
-export const store = configureStore({
-  reducer: {
+
+const rootReducers = combineReducers({
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
     student: studentReducer
-  },
+  })
+
+export type RootState = ReturnType<typeof rootReducers>;
+
+const appReducers = (
+  state: RootState | undefined,
+  action: UnknownAction
+) : RootState => {
+  if (action.type === "auth/logOut") {
+    state = undefined;
+  }
+
+  return rootReducers(state, action);
+}
+
+
+export const store = configureStore({
+  reducer: appReducers,
   middleware: getDefaultMiddleware => 
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true // set to false in production
 }); 
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
