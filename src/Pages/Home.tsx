05fc0f03ce07c34c89f1b7bd16887a459f0198ad
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-19 23:30:06 +0800

    Remove the search input from the header of the home component. Then add styling to make the container on the top right of the page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,8 +3,7 @@ import SidebarLayout from "../components/Common/SidebarLayout";
 import { useAppSelector } from "@/features/hooks";
 import { selectCurrentUser } from "@/features/Auth/authSlice";
 
-import { User, Search } from 'lucide-react';
-import { Input } from "@/components/ui/input";
+import { User } from 'lucide-react';
 
 export default function Home() {
   const fullname = useAppSelector(selectCurrentUser);
@@ -15,20 +14,9 @@ export default function Home() {
         <div className="flex flex-col h-full bg-gray-50">
 
           {/* header */}
-          <div 
-            className="h-16 px-10 flex items-center justify-between"
-          >
-            <div className="relative">
-              <Search size={20} className="text-gray-600 absolute left-2 top-2"/>
-              <Input 
-                type="text"
-                name="search"
-                placeholder="Search student by name"
-                className="w-80 bg-white shadow-none pl-8"
-              />
-            </div>
-            <div className="flex items-center gap-3">
-              <span>Hi, Ma'am {fullname?.split(" ")[0]}</span>
+          <div className="h-16 px-10 place-self-end content-center">
+            <div className="flex items-center gap-3 ">
+              <span>Hi, Teacher {fullname?.split(" ")[0]}</span>
               <User size={25} className="text-gray-500"/>
             </div>
           </div>
