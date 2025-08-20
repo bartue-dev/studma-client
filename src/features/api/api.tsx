4241e90f6f31bb57123a93a8ec964017c4dccdd0
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-20 22:45:20 +0800

    Remove the BaseQueryFn types

--- src/features/api/api.tsx
+++ src/features/api/api.tsx
@@ -15,11 +15,7 @@ const baseQuery = fetchBaseQuery({
   }
 });
 
-const baseQueryWithReauth: BaseQueryFn<
-  string | FetchArgs,
-  unknown,
-  FetchBaseQueryError
-> = async (args, api, extraOptions) => {
+const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions)
 
   console.log("REAUTH",result)
