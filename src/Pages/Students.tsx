6e1b6a120121d227ca7685379a3598c160d61b39
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-20 22:42:39 +0800

    Remove the StudentDataSlice redux state becuase it is uncessary since im using RTK

--- src/Pages/Students.tsx
+++ src/Pages/Students.tsx
@@ -2,18 +2,27 @@ import AddStudentDialog from "@/components/Common/AddStudentDialog";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
 
-import { studentDataSlice } from "@/features/Student/StudentSlice"
-import { useAppSelector } from "@/features/hooks"
+import { BeatLoader } from "react-spinners";
+import { Ellipsis } from "lucide-react";
+
+import useStudentData from "@/hooks/useStudentData";
 
 export default function Students() {
-  const students = useAppSelector(studentDataSlice);
+  const { 
+    students,
+    isSuccess,
+    isLoading,
+    isError,
+    apiError
+  } = useStudentData();
+
+  console.log("STUDENTS COMPONENT",students)
 
   return (
     <div>
@@ -21,9 +30,11 @@ export default function Students() {
         <h1 className="text-3xl text-gray-800 font-semibold">Students:</h1>
         <AddStudentDialog/>
       </div>
-      <div className="border shadow-sm bg-white p-3 rounded-md">
+      <div 
+        className="border shadow-sm bg-white p-3 rounded-md h-[480px] overflow-auto"
+        style={{ scrollbarWidth: "thin"}}
+      >
         <Table className="w-full h-full">
-          <TableCaption>List of Students</TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead className="w-[200px]">STUDENT NAME</TableHead>
@@ -31,18 +42,38 @@ export default function Students() {
               <TableHead className="text-center">SECTION</TableHead>
               <TableHead className="text-center">TOTAL ABSENCES</TableHead>
               <TableHead className="text-center">TOTAL PRESENT DAYS</TableHead>
+              <TableHead className="text-right"></TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {students.studentData.map((student) => (
-              <TableRow key={student.studentId}>
-                <TableCell className="font-medium">{student.firstname} {student.lastname}</TableCell>
-                <TableCell className="text-center">{student.grade}</TableCell>
-                <TableCell className="text-center">{student.section}</TableCell>
-                <TableCell className="text-center">test</TableCell>
-                <TableCell className="text-center">test</TableCell>
-              </TableRow>
-            ))}
+            {isLoading
+              ? <TableRow>
+                  <TableCell colSpan={5} className="text-center py-8 justify-items-center">
+                    <BeatLoader color="#5e9fe3"/>
+                  </TableCell>
+                </TableRow>
+              : isError
+                ? <TableRow>
+                  <TableCell colSpan={5} className="text-center py-8 text-red-500">
+                    {apiError.data.message}
+                  </TableCell>
+                </TableRow>
+              : isSuccess
+              && students?.studentData.map((student) => (
+                <TableRow 
+                  key={student.studentId}
+                  className="h-15"
+                >
+                  <TableCell className="font-medium">{student.firstname} {student.lastname}</TableCell>
+                  <TableCell className="text-center">{student.grade}</TableCell>
+                  <TableCell className="text-center">{student.section}</TableCell>
+                  <TableCell className="text-center">test</TableCell>
+                  <TableCell className="text-center">test</TableCell>
+                  <TableCell className="text-right cursor-pointer">
+                    <Ellipsis />
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </div>
