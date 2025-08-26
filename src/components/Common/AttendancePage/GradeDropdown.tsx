d3bec339b4129c21646831ea39d1080a0625f95c
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-26 23:49:58 +0800

    CReates AttendancePage folder where store the components for attendance parent component

---
+++ src/components/Common/AttendancePage/GradeDropdown.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import useStudentData from "@/hooks/useStudentData";
+
+type PropsTypes = {
+  setSelectGrade: (grade: number) => void
+}
+
+import { ChevronDown } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export function GradeDropDown({ setSelectGrade }: PropsTypes) {
+  const {students} = useStudentData({})
+  const [grade, setGrade] = useState<number[]>([])
+
+  useEffect(() => {
+    if (grade.length === 0) {
+      const mapGrade = students?.map(student => student.grade);
+      setGrade([...new Set(mapGrade)].sort((a, b) => a - b))
+    }
+  }, [grade.length, students])
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild 
+        className="[&:focus]:outline-none [&:focus]:ring-0 [&[data-state=open]]:outline-none"
+      >
+        <Button 
+          variant="ghost"
+          className="cursor-pointer"
+        >
+          <ChevronDown/>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-5" align="start">
+        <DropdownMenuLabel>Grades</DropdownMenuLabel>
+        <DropdownMenuSeparator/>
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            onSelect={() => setSelectGrade(0)}
+          >
+            All grades
+          </DropdownMenuItem>
+          {grade?.map((grade, i) => (
+            <DropdownMenuItem 
+              key={i}
+              onSelect={() => setSelectGrade(grade)}
+            >
+              {grade}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
